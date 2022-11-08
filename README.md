b0861cd6210e3481025e655500d927aae46c90bd
imagineazhar <2muhammadazhar@gmail.com>
2022-11-08 21:34:17 +0500

    🌟 Day 08

--- README.md
+++ README.md
@@ -18,49 +18,56 @@ My contributions to the [#30DayMapChallenge 2022](https://twitter.com/tjukanov/s
 * Day 05: [Ukraine](https://public.tableau.com/app/profile/m.azhar/viz/UkrainePorts30DayMapChallenge/ports)
 * Day 06: [Network](contributions/06-Network)
 * Day 07: [Raster](contributions/07-Raster)
+* Day 08: [OpenStreetMap](contributions/08-OpenStreetMap)
 
 </details>
 
 ***
 
-## Day 01 | [Points](https://public.tableau.com/app/profile/m.azhar/viz/BikeCollisionsinLondon30DaysMapChallengePoints/Day_01Points)
+### Day 01 | [Points](https://public.tableau.com/app/profile/m.azhar/viz/BikeCollisionsinLondon30DaysMapChallengePoints/Day_01Points)
 
 ![Bike Collisions](contributions/01-Points/01-points.png)
 
-***
+***OpenStreetMap
 
-## Day 02 | [lines](contributions/02-Lines/)
+### Day 02 | [lines](contributions/02-Lines/)
 
 ![Bike Collisions](contributions/02-Lines/02-lines.png)
 
 ***
 
-## Day 03 | [Polygon](contributions/03-polygons)
+### Day 03 | [Polygon](contributions/03-polygons)
 
 ![Parks in London](contributions/03-polygons/02-polygons.png)
 
 ***
 
-## Day 04 | [Green](contributions/04-Green)
+### Day 04 | [Green](contributions/04-Green)
 
 ![Parks in London](contributions/04-Green/04-green.png)
 
 ***
 
-## Day 05 | [Ukraine](https://public.tableau.com/app/profile/m.azhar/viz/UkrainePorts30DayMapChallenge/ports)
+### Day 05 | [Ukraine](https://public.tableau.com/app/profile/m.azhar/viz/UkrainePorts30DayMapChallenge/ports)
 
 ![Ukraine](contributions/05-Ukraine/ports.png)
 
 ***
 
-## Day 06 | [Network](contributions/06-Network/)
+### Day 06 | [Network](contributions/06-Network/)
 
 ![Network](contributions/06-Network/06-network.png)
 
 ***
 
-## Day 07 | [Raster](contributions/07-Raster)
+### Day 07 | [Raster](contributions/07-Raster)
 
 ![Raster](contributions/07-Raster/raster.png)
+
+***
+
+### Day 08 | [OpenStreetMap](contributions/08-OpenStreetMap)
+
+![OpenStreetMap](contributions/08-OpenStreetMap/08-osm.png)
 
 ***
---
+++ contributions/08-OpenStreetMap/osm.R
@@ -0,0 +1,121 @@
+library(tidyverse)
+library(sf)
+library(osmdata)
+library(sysfonts)
+library(showtext)
+
+# ------ Typography ------ 
+
+font_add_google("Cardo", "title_font")
+showtext_auto()
+
+title_font <- "title_font"
+
+
+# ------ Color palette ------ 
+
+bg_color <- "#f9ece3"
+shape_color <- "#fbf3d5"
+road_color <- "#1a1a1a"
+building_color <- "#5f6a86"
+park_color <- "#a6b1a1"
+
+
+# ------ Get Data ------ 
+
+bbx <- getbb("Islamabad","," ,"PK")
+
+# polygon shape
+shape <- getbb("Islamabad","," ,"PK",  format_out = "sf_polygon")
+shape<-shape[1,]
+
+# Roads data
+roads <- opq(bbx)|>
+  add_osm_feature(key = "highway", 
+                  value = c("motorway","trunk",
+                            "primary", "secondary",
+                            "tertiary", "motorway_link",
+                            "trunk_link", "primary_link",
+                            "secondary_link", "tertiary_link")) |>
+  osmdata_sf()
+
+# Get small streets
+streets <- opq(bbx) |>
+  add_osm_feature(key = "highway",
+                  value = c("residential", "living_street",  "service",
+                            "unclassified", "pedestrian", "footway",
+                            "track","path")) |>
+  osmdata_sf()
+
+# Get parks
+parks <- opq(bbx)|>
+  add_osm_feature(key = "leisure", value = "park")|>
+  osmdata_sf()
+
+# Circle - Center
+
+city_coords <- tibble(address = "Islamabad, PK")|>
+  tidygeocoder::geocode(address, method = 'osm', long = long, lat = lat)
+
+
+long <- city_coords$long
+lat<- city_coords$lat
+
+crs2 <- 24313 
+
+center_proj <- tibble(lat=lat, long=long)|>
+  st_as_sf(coords = c("long", "lat"), crs=4326)
+
+# circle crop
+
+distance <- 5500
+circle <- tibble(lat=lat, long=long) |>
+  st_as_sf(coords = c("long", "lat"), crs = 4326) |>
+  st_transform(crs = crs2) |>
+  st_buffer(dist = distance) |>
+  st_transform(crs = 4326)
+
+
+# filter data
+streets_lines <- st_intersection(circle, streets$osm_lines)
+roads_lines <- st_intersection(circle, roads$osm_lines)
+parks_polygons <- st_intersection(circle, parks$osm_polygons)
+
+
+
+# ------ Map ------ 
+ggplot()+
+  geom_sf(data = streets_lines,
+          col = road_color,
+          size = .4,
+          alpha = .65 ) +
+  
+  geom_sf(data = roads_lines,
+          col = road_color,
+          size = .6,
+          alpha = .8) +
+  geom_sf(data = parks_polygons,
+          fill=park_color,
+          color=park_color)+
+  labs(
+    caption = "<span style='font-size: 30px;margin-bottom: 35px;'>PAKISTAN</span><br><span style='font-size:85px;'>ISLAMABAD</span><br><br><span style='font-size:35px;'>33° 68' N - 73° 04' W</span><br><br>
+       <span style='font-size:14px;'>Data : OpenStreetMap  **  Graphic: @imagineazhar</span>"
+  ) +
+  theme_void()+
+  theme(
+    # CAPTION
+    plot.caption = ggtext::element_markdown(hjust = .5,
+                                            family = title_font,
+                                            face = "bold"), 
+    plot.background = element_rect(fill=bg_color, color=NA),
+    plot.margin = margin(b=20)
+  )
+
+
+
+# ------ Save Plot ------ 
+
+showtext_opts(dpi = 320) 
+ggsave("08-osm.png", height = 10,
+       width = 8, dpi=320)  
+showtext_auto(FALSE)
